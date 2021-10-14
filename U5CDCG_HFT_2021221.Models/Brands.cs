8970a9af0160a14392f08fbcedcd6b9e662de5e9
ViskiDaniel <u5cdcg@stud.uni-obuda.hu>
2021-10-14 16:21:39 +0200

    Models classes added

---
+++ U5CDCG_HFT_2021221.Models/Brands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U5CDCG_HFT_2021221.Models
+{
+    class Brands
+    {
+        [Key]
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int? Since { get; set; }
+        public List<string> Factories { get; set; }
+    }
+}
---
+++ U5CDCG_HFT_2021221.Models/Export.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U5CDCG_HFT_2021221.Models
+{
+    class Export
+    {
+        public string From { get; set; }
+        public string Destination { get; set; }
+        public int Amount { get; set; }
+    }
+}
