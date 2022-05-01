dd088f7af62dce529b17606556e08bb9ac8355a2
ViskiDaniel <u5cdcg@stud.uni-obuda.hu>
2022-05-01 21:01:33 +0200

    done

--- U5CDCG_GUI_2021222.JSClient/Startup.cs
+++ U5CDCG_GUI_2021222.JSClient/Startup.cs
@@ -7,12 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using U5CDCG_HFT_2021221.Data;
-using U5CDCG_HFT_2021221.Endpoint.Services;
-using U5CDCG_HFT_2021221.Logic;
-using U5CDCG_HFT_2021221.Repository;
 
-namespace U5CDCG_HFT_2021221.Endpoint
+namespace U5CDCG_GUI_2021222.JSClient
 {
     public class Startup
     {
@@ -20,16 +16,6 @@ namespace U5CDCG_HFT_2021221.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            services.AddTransient<IBookLogic, BookLogic>();
-            services.AddTransient<ICustomerLogic, CustomerLogic>();
-            services.AddTransient<ILibraryLogic, LibraryLogic>();
-            services.AddTransient<IBookRepository, BookRepository>();
-            services.AddTransient<ICustomerRepository, CustomerRepository>();
-            services.AddTransient<ILibraryRepository, LibraryRepository>();
-            services.AddTransient<LibraryDbContext, LibraryDbContext>();
-            services.AddSignalR();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,20 +27,18 @@ namespace U5CDCG_HFT_2021221.Endpoint
             }
 
             app.UseRouting();
-            app.UseCors(x => x
-               .AllowCredentials()
-               .AllowAnyMethod()
-               .AllowAnyHeader()
-               .WithOrigins("http://localhost:49201"));
+            app.UseStaticFiles();
+
+           
 
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
-                endpoints.MapHub<SignalRHub>("/hub");
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
-
-            
         }
     }
 }
---
+++ U5CDCG_GUI_2021222.JSClient/wwwroot/script.js
@@ -0,0 +1,145 @@
+﻿let books = [];
+let connection=null;
+getdata();
+setupsignalr();
+
+let bookidtoupdate = -1;
+
+async function getdata() {
+    await fetch('http://localhost:64653/book').then(x => x.json()).then(y => {
+        books = y;        
+        display();
+    });
+}
+
+function setupsignalr() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:64653/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on
+        (
+            "BookCreated", (user, message) => {
+                getdata();
+        });
+
+
+    connection.on
+        (
+            "BookDeleted", (user, message) => {
+                getdata();
+        });
+
+    connection.on
+        (
+            "BookUpdated", (user, message) => {
+                getdata();
+            });
+
+    connection.onclose
+        (async () => {
+            await start();
+        });
+    start();
+
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+function showupdate(id) {
+    document.getElementById('bookauthortoupdate').value = books.find(x => x['bookId'] == id)['author'];
+    document.getElementById('booktitletoupdate').value = books.find(x => x['bookId'] == id)['title'];
+    document.getElementById('updateformdiv').style.display = 'flex';
+    bookidtoupdate = id;
+}
+
+function display() {
+    document.getElementById('resultarea').innerHTML = "";
+    books.forEach(x => {
+        document.getElementById('resultarea').innerHTML += "<tr><td>" + x.author + "</td><td>" + x.title + "</td><td>" + `<button type="button" onclick="remove(${x.bookId})">Delete</button>` + `<button type="button" onclick="showupdate(${x.bookId})">Update</button>` + "</td></tr>"
+        console.log(x.title);
+    });
+}
+
+function remove(id){
+    fetch('http://localhost:64653/book/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body:null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+function create() {
+    let aauthor = document.getElementById("bookauthor").value;
+    let ttitle = document.getElementById("booktitle").value;
+
+    fetch('http://localhost:64653/book', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                author:aauthor,
+                title:ttitle
+            }),
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+    
+}
+
+function update() {
+    document.getElementById('updateformdiv').style = 'none';
+    let aauthor = document.getElementById("bookauthortoupdate").value;
+    let ttitle = document.getElementById("booktitletoupdate").value;
+
+    fetch('http://localhost:64653/book', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                author: aauthor,
+                title: ttitle,
+                bookId: bookidtoupdate
+            }),
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+
+}
